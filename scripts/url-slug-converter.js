4918f0293a13209ec307178db45961920c63a331
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-04-30 01:30:43 -0300

    Taking an string and returning an URL slug w/functional programming

--- scripts/url-slug-converter.js
+++ scripts/url-slug-converter.js
@@ -15,7 +15,10 @@ console.log(urlSlug(" Winter Is  Coming"));
 function urlSlug(title) {
   return title
     .toLowerCase()
+  // .trim removes the whitespaces from the start/end of the string.
     .trim()
+	// Split taking an regex (that splits the in an array using the whitespaces as delimiters).
     .split(/\s+/)
+  // Joining each word in the array in a string with '-' char.
     .join("-");
 }
