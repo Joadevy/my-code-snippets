5ccf694f9f7a6f9b5d7b7e1c418f9d04afb4ed70
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-05-12 16:07:45 -0300

    Sum all numbers in a Range

--- scripts/sum-number-in-a-range.js
+++ scripts/sum-number-in-a-range.js
@@ -1,20 +1,30 @@
+// The functon receives two numbers in an array, then return the sum of those two numbers plus the sum of all the numbers between them. The lowest number will not always come first.
+
 function sumAll(arr) {
-  const plusBoth = arr.reduce((acum,number) => {
-    return acum +=number;
-  },0);
-  let min,max;
-  if (arr[0]>arr[1]){
-    min = arr[1];
-    max = arr[0];
-  } else {
-    min = arr[0];
-    max = arr[1];
-  }
+ const range = arr.reduce((data,number)=>{
+    data.min>number ? data.min = number : '';
+    data.max<number ? data.max = number : '';
+   return data;
+  },{min:arr[0],max:arr[0]});
+  
   let plusBetween = 0;
-  for (let min; min<max;min++){
-    plusBetween+=min;
+  
+  for (let number =range.min;number<=range.max;number++){
+    plusBetween+=number;
   }
-  return plusBetween+plusBoth
+  return plusBetween;
 }
 
-console.log(sumAll([4, 1]));
+console.log('Final result: ',sumAll([1, 2]));
+
+// An easier or more readable soluton would look like:
+
+function sumAll(arr){
+  let max = Math.max(arr[0], arr[1]);
+  let min = Math.min(arr[0], arr[1]);
+  let sumBetween = 0;
+  for (let number = min;number<=max;number++){
+    sumBetween+=number;
+  }
+  return sumBetween;
+}
