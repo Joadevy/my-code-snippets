5c0ebeae83a1eddaa1c1285757e017177f69d3ec
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-07-04 01:05:00 -0300

    Sorting the array fixed

--- scripts/smallest-common-multiple.js
+++ scripts/smallest-common-multiple.js
@@ -2,7 +2,11 @@
 
 function smallestCommons(arr){
   // Sorting to reach the max value in arr[1] && the min in arr[0]
-  arr.sort()
+ console.log(arr);
+		arr.sort((a,b)=>{
+				return a === b ? 0 : a>b ? 1 : -1
+		});
+console.log(arr)
   let founded= false; // Keep control if the number is founded.
   let commonMultiple = 1; // To store the different minMultiples before meet the range condition.
   let counter = 1; // To calculate the multiples.
@@ -14,6 +18,8 @@ function smallestCommons(arr){
 			if (founded){
 				console.log('Probando con ' + commonMultiple);
 				let notMultiple = false;
+				console.log(arr[0]);
+				console.log(notMultiple);
 				arr.reduce((acum) => {
         while(acum<arr[1] && !notMultiple){
 						console.log(acum);
@@ -21,6 +27,7 @@ function smallestCommons(arr){
           acum++	
         }
       },arr[0]+1)
+				console.log(notMultiple);
 					if (!notMultiple){
 						console.log('el valor  ' + commonMultiple + ' cumple la prop' );
 				} else {
@@ -34,6 +41,6 @@ function smallestCommons(arr){
 		
 // Testing/
 let array = [23,18]
-smallestCommons(array)
-
+smallestCommons([10,2])
+		
 // Hacer un ammend para editar el commit anterior.
