21fad3a209883a78d0910dfb36715fb626ac253c
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-07-04 00:18:36 -0300

    Doing the tests fine

--- scripts/smallest-common-multiple.js
+++ scripts/smallest-common-multiple.js
@@ -7,26 +7,33 @@ function smallestCommons(arr){
   let commonMultiple = 1; // To store the different minMultiples before meet the range condition.
   let counter = 1; // To calculate the multiples.
   while (founded === false){
-    console.log('Este es el acum: '+ commonMultiple)
   // Calculating the common multiples.
     commonMultiple = arr[1] * counter;
-    commonMultiple % arr[0] == 0 ? founded = true : ''
+		commonMultiple % arr[0] == 0 ? founded = true : ''
   // Once a minMultiple is founded then need to check the range condition
-    if (founded){
-      arr.reduce((acum) => {
-        while(acum<arr[1]){
-          console.log(acum)
-          acum++
+			if (founded){
+				console.log('Probando con ' + commonMultiple);
+				let notMultiple = false;
+				arr.reduce((acum) => {
+        while(acum<arr[1] && !notMultiple){
+						console.log(acum);
+						commonMultiple % acum == 0 ? '' : notMultiple = true;      
+          acum++	
         }
       },arr[0]+1)
+					if (!notMultiple){
+						console.log('el valor  ' + commonMultiple + ' cumple la prop' );
+				} else {
+						founded = false;
+				}
     }
     counter++
   }
   return commonMultiple
 }
-
-// Testing
-let array = [2,6]
+		
+// Testing/
+let array = [23,18]
 smallestCommons(array)
 
 // Hacer un ammend para editar el commit anterior.
