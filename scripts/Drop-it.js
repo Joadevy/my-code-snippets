fc1642e4291a2e51f0dc6ce97eb50d82a675edb7
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-07-05 01:56:14 -0300

    First solution Working && passing the tests

---
+++ scripts/Drop-it.js
@@ -0,0 +1,19 @@
+// Given the array arr, iterate through and remove each element starting from the first element (the 0 index) until the function func returns true when the iterated element is passed through it.
+
+// Then return the rest of the array once the condition is satisfied, otherwise, arr should be returned as an empty array.
+
+function dropElements(arr, func) {
+// While there are something in the array and the func with the first element returns false.
+  while(!func(arr[0]) && arr.length){
+    // Removes the first element of the array.
+   	arr.shift()
+  }
+  return arr
+}
+
+// Testing
+dropElements([0,1,0,1], function(n) {return n === 1; });
+// It returns [1,0,1] because 1 === 1 returns true && the function stops.
+
+dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;});
+// It returns [3,9,2] cause 3>2 returns true && the function stops.
