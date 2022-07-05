3c44daedcb1804c81e43a826717ed76dbadf33f8
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-07-05 02:10:20 -0300

    Shitfting items til a function's return condition

--- scripts/Drop-it.js
+++ scripts/Drop-it.js
@@ -19,3 +19,16 @@ dropElements([0,1,0,1], function(n) {return n === 1; });
 
 dropElements([1, 2, 3, 9, 2], function(n) {return n > 2;});
 // It returns [3,9,2] cause 3>2 returns true && the function stops.
+
+//	*** Another approach to solve the problem ***
+
+function dropsElements(arr, func) {
+	// Find the index of the element that passes the condition (it will used to stop removing elements) 	
+  let sliceIndex = arr.findIndex(func);
+	console.log(sliceIndex);	
+  // If arr.findIndex has no matches then return -1. If sliceIndex < 1 removes the elements with slice (it didn't mutate arr)
+  return arr.slice(sliceIndex >= 0 ? sliceIndex : arr.length);
+}
+
+dropsElements([1, 2, 3, 9, 2], function(n) {return n > 2;});
+// It returns [3,9,2] cause arr.findIndex returns 2 & the arr.slice(2) returns it.
