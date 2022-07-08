97350d3fe0a2d4a3474ad2e277c5d653df84058a
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-07-07 22:34:02 -0300

    Flatten arrays with recursion

--- scripts/steamroller.js
+++ scripts/steamroller.js
@@ -1,14 +1,20 @@
 // Flatten a nested array. e.g. [[["a"]], [["b"]]] must return ["a", "b"]
 
+// First approach to solve the problem
 function steamrollArray(arr){
 		return arr.reduce((result,element)=> {
+				// Checking if the element is an array.
 				if (Array.isArray(element)) {
+						// Uses recursion to go deep and concatenates the output to the reduce acumulator (result)
 					return result.concat(steamrollArray(element))
 				} else {
+						// If the element is not an array, it pushes into the reduce's acumulator.
 						result.push(element)	
 				}
 				return result
 		},[]);
 }
 
+// Testing
 console.log(steamrollArray([1, {}, [3, [[4]]]])) 
+// It returns [1,{},3,4]
