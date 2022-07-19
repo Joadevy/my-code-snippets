debe424b87281847aaead67ec82d3227d86052bb
Joaquin Arlettaz <jjoaquinarlettaz@gmail.com>
2022-07-19 01:08:11 -0300

    Valid US phone-number checker

--- scripts/phone-number-validator.js
+++ scripts/phone-number-validator.js
@@ -1,15 +1,12 @@
-// Return true if the passed string looks like a valid US phone number.
-// The user may fill out the form field any way they choose as long as it has the format of a valid US number
+/* 	VALID US PHONE FORMAT CHECKER
 
-/* Valid formats:
-555-555-5555
-(555)555-5555
-(555) 555-5555
-555 555 5555
-5555555555
-1 555 555 5555 */
+Return true if the passed string looks like a valid US phone number.
+ The user may fill out the form field any way they choose as long as it has the format of a valid US number
 
-/* 
+	Valid formats:
+555-555-5555 | (555)555-5555 | (555) 555-5555 | 555 555 5555 | 5555555555 | 1 555 555 5555 
+
+
 Strategy that I thought to solve the problem:
 
 Clean the string and Then check if it has length of 10 or 11, if not then return false.
@@ -22,8 +19,8 @@ If length is 10 or 11 then: try a regex starting with 1 | nothing:
 
 function telephoneCheck(str) {
   let onlyNumbers = /[0-9]/;
-  let cleaningStr = str.split('').filter(element => element.match(onlyNumbers));
-  if (cleaningStr.length == 10 || cleaningStr.length ==11){    
+  let cleanStr = str.split('').filter(element => element.match(onlyNumbers));
+  if (cleanStr.length == 10 || cleanStr.length ==11){    
     let regex = /^1? ?[(]\d{3}[)]\s?\d{3}[-]?\d{4}$|^1?\s?\d{3}\s?\d{3}\s?\d{4}$|^1?\s?\d{3}[-]\d{3}[-]\d{4}$/
      if (regex.test(str)){
        return true
