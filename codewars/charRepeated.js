687f446f2c13a5ba148ce4af27e658f8ea0c499a
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-08-13 01:30:30 -0300

    Char repeated counter

---
+++ codewars/charRepeated.js
@@ -0,0 +1,18 @@
+function duplicateCount(text){
+  str = text.toUpperCase(); // To avoid mutation of the text
+  return str.split('').reduce((repeats,char)=>{
+    // Iterating through the next index of the actual char to the end of text
+    for (let index=str.indexOf(char)+1;index<str.length;index++){
+      // if there is another same char than actual of the reduce
+    	if (str.charAt(index) == char){
+        // Increments the repeats counter.
+        repeats++;
+        // Removing all the other same chars in text because they don't matter anymore.
+        for (let letter in str){
+           str = str.replace(char,''); 
+        }
+      }
+    }
+    return repeats
+  },0)
+}
