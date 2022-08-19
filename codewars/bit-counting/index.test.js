f7ce6a90c5113936528cfcc4fa9e3fa15d3b0857
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-08-19 01:22:56 -0300

    Tests cleaned

--- codewars/bit-counting/index.test.js
+++ codewars/bit-counting/index.test.js
@@ -10,15 +10,3 @@ describe("Basic tests", () => {
         expect(countBits(10)).toEqual(2);
   });
 });
-
-
-// describe("Basic tests", () => {
-//     it("Testing for fixed tests", () => {
-//       assert.strictEqual(countBits(0), 0);
-//       assert.strictEqual(countBits(4), 1);
-//       assert.strictEqual(countBits(7), 3);
-//       assert.strictEqual(countBits(9), 2);
-//       assert.strictEqual(countBits(10), 2);
-//       })
-//     })
-  
