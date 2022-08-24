a9586336f3394de513188db2430fb1894c35d224
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-08-24 01:58:56 -0300

    Tests passed

--- codewars/like-dislike/index.test.js
+++ codewars/like-dislike/index.test.js
@@ -1,22 +1,19 @@
 import { expect, describe, it } from "vitest";
 import { likeOrDislike} from "./index.js";
 
-
-//  TO DO: build the tests!
-
-// describe("Likes vs. Dislikes",() => {
-//   it("Should return Dislike, Like or Nothing according to the last button status",() => {
-//     expect(likeOrDislike([]).toBe(Dislike]) Dislike );
-//     expect(likeOrDislike([]).toBe(Like,Like]) Nothing );
-//     expect(likeOrDislike([]).toBe(Dislike,Like]) Like );
-//     expect(likeOrDislike([]).toBe(Like,Dislike,Dislike]) Nothing );
-//   });
-//   it("more example tests",() => {
-//     expect(likeOrDislike([]).toBe(Dislike,Dislike]) Nothing );
-//     expect(likeOrDislike([]).toBe(Like,Like,Like]) Like );
-//     expect(likeOrDislike([]).toBe(Like,Dislike]) Dislike );
-//     expect(likeOrDislike([Dislike,Like,Dislike]).toBe(Dislike,Like,Dislike]) Dislike );
-//     expect(likeOrDislike([Like,Like,Dislike,Like,Like,Like,Like,Dislike]).toBe('Dislike');
-//     expect(likeOrDislike([])).toBe( Nothing );
-//   });
-// });
+describe("Likes vs. Dislikes",() => {
+  it("Should return Dislike, Like or Nothing according to the last button status",() => {
+    expect(likeOrDislike(['Dislike'])).toBe('Dislike');
+    expect(likeOrDislike(['Like','Like'])).toBe('Nothing');
+    expect (likeOrDislike(['Dislike','Like'])).toBe('Like');
+    expect(likeOrDislike(['Like','Dislike','Dislike'])).toBe('Nothing');
+  });
+  it("more tests",() => {
+    expect(likeOrDislike(['Dislike','Dislike'])).toBe('Nothing');
+    expect(likeOrDislike(['Like','Like','Like'])).toBe('Like');
+    expect(likeOrDislike(['Like','Dislike'])).toBe('Dislike');
+    expect(likeOrDislike(['Dislike','Like','Dislike'])).toBe('Dislike');
+    expect(likeOrDislike(['Like','Like','Dislike','Like','Like','Like','Like','Dislike'])).toBe('Dislike');
+    expect(likeOrDislike([])).toBe('Nothing');
+  });
+});
