7aa600a0c10d73421e803cba79406b3a2e9813e0
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-08-26 02:04:37 -0300

    Sudoku finished? Checker

--- codewars/sudoku-finished/README.md
+++ codewars/sudoku-finished/README.md
@@ -1,3 +1,5 @@
+## SUDOKU CHECKER!
+
 Write a function done_or_not/DoneOrNot passing a board (list[list_lines]) as parameter. If the board is valid return 'Finished!', otherwise return 'Try again!'
 
 Sudoku rules:  
--- codewars/sudoku-finished/index.js
+++ codewars/sudoku-finished/index.js
@@ -1,24 +1,18 @@
+// I KNOW, I HAVE FOR-LOOPING ADDICTION. DON'T JUDGE ME, I'LL BE OK AND THE TESTS PASSED.
+
 export const doneOrNot = (board) => {
-    if (allRowsOK(board) && allColumnsOK(board)){
-        return 'All going well'
+    if (allRowsOK(board) && allColumnsOK(board) && allBoxesOK(board)){
+        return 'Finished!'
     } else {
-        return 'Try Again!'
+        return 'Try again!'
     }
 }
 
-// Auxiliar function that checks for equalit of the arrays.
-function arrayEquals(a, b) {
-    return Array.isArray(a) &&
-      Array.isArray(b) &&
-      a.length === b.length &&
-      a.every((val, index) => val === b[index]);
-  }
 
 function allRowsOK (board) {
     for (let row = 0 ; row < 9 ; row ++){
         let rowChecking = board[row]
-        console.log(rowChecking)
-        if (!allNumbersIn(rowChecking)) return false;
+        if (!allNumbersIn(rowChecking)) return false;  // Check if all numbers are in the row.
         for (let subRow = row+1; subRow < 9 ; subRow++){
             if (arrayEquals(rowChecking,board[subRow])){ 
                 return false
@@ -28,11 +22,22 @@ function allRowsOK (board) {
     return true
 }
 
+function allNumbersIn (line){
+    const numbers = [1,2,3,4,5,6,7,8,9];
+    return numbers.every(number => line.includes(number))
+}
+
+function arrayEquals(a, b) {
+    return Array.isArray(a) &&
+      Array.isArray(b) &&
+      a.length === b.length &&
+      a.every((val, index) => val === b[index]);
+  }
+
 function allColumnsOK(board){
     for (let column = 0 ; column < 9 ; column ++){
         const columnChecking = loadColumn(column,board);
-        console.log(columnChecking);
-        if (!allNumbersIn(columnChecking)) return false;
+        if (!allNumbersIn(columnChecking)) return false; // Check if all numbers are in the column.
         for (let subColumn = column+1; subColumn < 9 ; subColumn++){
             const nextColumn = loadColumn(subColumn,board);
             if (arrayEquals(columnChecking,nextColumn)){ 
@@ -43,37 +48,6 @@ function allColumnsOK(board){
     return true
 }
 
-
-// Necesito los 3 primeros elementos de cada tres filas.
-function allBoxesOK(boardImmutable){
-    let board = boardImmutable;
-    for (let boxesControl = 0 ; boxesControl <3 ; boxesControl++){
-        for (let boxLine = 0; boxLine < 9 ; boxLine+=3){
-           let boxChecking = loadBox(boxLine,board);
-           console.log(boxChecking);
-        }   
-    }
-    return true
-}
-
-function loadBox(boxLine,board){
-    let arr = []
-    for (let line = boxLine; line < boxLine+3 ; line++){
-        // 1
-        // 2 
-        // 3
-        for (let numbers = 0; numbers < 3 ; numbers++){
-            arr.push(board[line].shift());
-        }
-    }
-    return arr
-}
-
-function allNumbersIn (line){
-    const numbers = [1,2,3,4,5,6,7,8,9];
-    return numbers.every(number => line.includes(number))
-}
-
 function loadColumn(column,board) {
     const arr = []
     for (let row = 0 ; row < 9 ; row++){
@@ -82,38 +56,27 @@ function loadColumn(column,board) {
     return arr
 }   
 
+function allBoxesOK(boardImmutable){
+    let board = boardImmutable;
+    const arrayBoxes = [];
+    for (let boxesControl = 0 ; boxesControl <3 ; boxesControl++){
+        for (let boxLine = 0; boxLine < 9 ; boxLine+=3){
+           let boxChecking = loadBox(boxLine,board);
+           if (!allNumbersIn(boxChecking)) return false // Check if all numbers are in the box.
+            arrayBoxes.push(boxChecking); // Creates an array with all the 3x3 boxes.
+        }   
+    }
 
-// Se debe checkear que:
-// * Todos los numeros esten en cada columna, renglon y cuadro 3x3.
-// * Ningun cuadro 3x3 sea igual a otro.
+    // Checking for 3x3 boxes are all differents.
+    return allRowsOK(arrayBoxes)
+}
 
-// DONE Ningun renglon sea igual a otro.
-// DONE Ninguna columna sea igual a otra.
-// => Checkear al mismo tiempo que vemos si es diferente la columna que esten todos los numeros en esa columna.
-
-// Tiene pinta de que hay que checkear con unos cuantos FOR, la pregunta es que chequear primero para mejorar eficiencia?
-
-
-// console.log(doneOrNot([
-//     [5, 3, 4, 6, 7, 8, 9, 1, 2], 
-//     [6, 7, 2, 1, 9, 5, 3, 4, 8],
-//     [1, 9, 8, 3, 4, 2, 5, 6, 7],
-//     [8, 5, 9, 7, 6, 1, 4, 2, 3],
-//     [4, 2, 6, 8, 5, 3, 7, 9, 1],
-//     [7, 1, 3, 9, 2, 4, 8, 5, 6],
-//     [9, 6, 1, 5, 3, 7, 2, 8, 4],
-//     [2, 8, 7, 4, 1, 9, 6, 3, 5],
-//     [3, 4, 5, 2, 8, 6, 1, 7, 9]
-// ]))
-
-console.log(allBoxesOK([
-    [5, 3, 4, 6, 7, 8, 9, 1, 2], 
-    [6, 7, 2, 1, 9, 5, 3, 4, 8],
-    [1, 9, 8, 3, 4, 2, 5, 6, 7],
-    [8, 5, 9, 7, 6, 1, 4, 2, 3],
-    [4, 2, 6, 8, 5, 3, 7, 9, 1],
-    [7, 1, 3, 9, 2, 4, 8, 5, 6],
-    [9, 6, 1, 5, 3, 7, 2, 8, 4],
-    [2, 8, 7, 4, 1, 9, 6, 3, 5],
-    [3, 4, 5, 2, 8, 6, 1, 7, 9]
-]))
+function loadBox(boxLine,board){
+    let arr = []
+    for (let line = boxLine; line < boxLine+3 ; line++){
+        for (let numbers = 0; numbers < 3 ; numbers++){
+            arr.push(board[line].shift());
+        }
+    }
+    return arr
+}
