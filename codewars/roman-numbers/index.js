b7c3c6d9fd2c201be610bea46ca76b8673328473
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-08-29 00:28:33 -0300

    Better approach added

--- codewars/roman-numbers/index.js
+++ codewars/roman-numbers/index.js
@@ -1,3 +1,46 @@
+// My shorter approach (not my initial solution)
+const converterTable = {
+    1000: 'M',
+    900:  'CM',
+    500:  'D',
+    400:  'CD',
+    100:  'C',
+    90:   'XC',
+    50:   'L',
+    40:   'XL',
+    10:   'X',
+    9:    'IX',
+    5:    'V',
+    4:    'IV',
+    1:    'I'
+}
+export class RomanNumerals {
+    // Making the static methods for allow only the class to answer it.
+    static toRoman(integer){
+       let romanNumber = '';
+       let keys = Object.keys(converterTable).sort((a,b) =>{ return a-b ? -1 : 1 }); 
+        for (let key of keys){
+            while(integer - key >= 0){
+                integer-=key;
+                romanNumber += converterTable[key];
+            }
+        }   
+        return romanNumber
+    }
+
+    static fromRoman(roman){
+        // Uses a regex and match (with g flag) to store in an array all of the matches (that are the same of the values in the converterTable)
+        return roman.match(/CM|CD|XC|XL|IX|IV|\w/g).reduce((integer,roman) => {
+            // Iterating through the values for each element matched of the roman input (this could improve efficiency)
+            for (const number in converterTable){
+                converterTable[number] == roman ? integer += parseInt(number) : ''
+            }
+            return integer;
+        },0);
+    }
+}
+
+// My initial idea:
 class romanConverter {
     constructor(){
         this.converterTable = {
@@ -78,4 +121,4 @@ class romanConverter {
     }
 }
 
-export const RomanNumerals = new romanConverter();
+// export const RomanNumerals = new romanConverter();
