a485d57332103cd9d479aea436f055727626a46e
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-08-31 01:30:07 -0300

    Documentation improved

--- codewars/4kyu/nesting-structure-comparision/README.md
+++ codewars/4kyu/nesting-structure-comparision/README.md
@@ -1,4 +1,4 @@
-Complete the function/method (depending on the language) to return true/True when its argument is an array that has the same nesting structures and same corresponding length of nested arrays as the first array.
+Return true/True when its argument is an array that has the same nesting structures and same corresponding length of nested arrays as the first array.
 
 For example:
 ``` javascript
--- codewars/4kyu/nesting-structure-comparision/index.js
+++ codewars/4kyu/nesting-structure-comparision/index.js
@@ -4,13 +4,16 @@ export default Array.prototype.sameStructureAs = function (other) {
         return false;
       }
     
-    // Then checks the elements into
+    // Then checks the elements inside
       for (const element in this) {
+        // If the two elements are arrays then use recursion to check the inner structure.
         if (Array.isArray(this[element]) && Array.isArray(other[element])) {
             if (!this[element].sameStructureAs(other[element])) return false
-          } 
+        } 
+        // Checks if the two elements have different structure: arrays or something else 
         else if (!Array.isArray(this[element]) && Array.isArray(other[element])) return false;
         else if (Array.isArray(this[element]) && !Array.isArray(other[element])) return false;
       }
       return true
   };
+
