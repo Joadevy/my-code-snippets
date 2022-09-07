d37b86c1377b761f0e4ce29eb0a895a3e7a03082
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-09-07 02:35:16 -0300

    Returning the range but nod the others

--- codewars/4kyu/range-extraction/index.js
+++ codewars/4kyu/range-extraction/index.js
@@ -1,26 +1,27 @@
 function solution(list){
     return list.reduce((numbers,number,index,arr) => {
-        if (!index){
-            arr[index+1] == number+1 ? numbers[0].push(number) : numbers[1] += number;
+        if (numbers[0].length === 0){
+            console.log('pusheo',number)
+            numbers[0].push(number);
         } else if (numbers[0]){
-            arr[index+1] === number+1 && arr[index-1] === number-1 ?  
-                numbers[0].push(number) : 
-                // Estaria en [1,2,3,5,6] situacion
-                // numbers[1] += doRange(numbers[0]) // Deberia devolver el 1-3
-                number == numbers[0][number.length]+1 ? numbers[0].push(number) :
-                (numbers[1] += (numbers[0].toString()) + ',' + number, numbers[0] = [])
-        } else {
-            numbers[1] += ',' + number
+            arr[index-1] === number-1 ?  
+                (numbers[0].push(number),
+                index == arr.length-1 && numbers[0].length > 2 ? numbers[1] += doRange(numbers[0]) : '') : 
+                // Estaria en [1,2,3,5,6] situacion, es decir hay un corte.
+                numbers[0].length > 2 ? (numbers[1] += doRange(numbers[0]), numbers[0] = []) :
+                // Devuelvo el rango SOLO SI HAY UN CORTE pero necesito volver a analizar
+                 (numbers[1] += (numbers[0].toString()), 
+                 numbers[0] = []);
+        } 
+        else {
+            `${numbers[1]},${number}`
         }
         return numbers
     },[[],""])    
 }
 
-[1,2,3]
-[1,2,4,5]
-
 function doRange(arr){
     return`${arr[0]}-${arr[arr.length-1]}` 
 }
 
-console.log(solution([1,2,3,4,5,6,9]))
+console.log(solution([1,2,3,4,5,6,7,8,9,11,12]))
