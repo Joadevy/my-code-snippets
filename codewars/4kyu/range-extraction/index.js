be1c41705c319aa637b5cf8c5475f0d9ced690c0
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-09-10 01:40:51 -0300

    Code working fine

--- codewars/4kyu/range-extraction/index.js
+++ codewars/4kyu/range-extraction/index.js
@@ -1,27 +1,24 @@
-function solution(list){
-    return list.reduce((numbers,number,index,arr) => {
-        if (numbers[0].length === 0){
-            console.log('pusheo',number)
-            numbers[0].push(number);
-        } else if (numbers[0]){
-            arr[index-1] === number-1 ?  
-                (numbers[0].push(number),
-                index == arr.length-1 && numbers[0].length > 2 ? numbers[1] += doRange(numbers[0]) : '') : 
-                // Estaria en [1,2,3,5,6] situacion, es decir hay un corte.
-                numbers[0].length > 2 ? (numbers[1] += doRange(numbers[0]), numbers[0] = []) :
-                // Devuelvo el rango SOLO SI HAY UN CORTE pero necesito volver a analizar
-                 (numbers[1] += (numbers[0].toString()), 
-                 numbers[0] = []);
-        } 
-        else {
-            `${numbers[1]},${number}`
-        }
+export function solution(list){
+    return list.reduce((numbers,number,index,arr)=>{
+            if (number === arr[index+1]-1){
+                numbers[0].push(number);
+            } else if (numbers[0].length>1){
+                numbers[0].push(number)
+                numbers[1].length ? numbers[1]+=`,${doRange(numbers[0])}` : numbers[1]+=`${doRange(numbers[0])}`;
+                numbers[0] = [];
+            } else if (numbers[0].length){
+                numbers[0].push(number);
+                numbers[1].length ? numbers[1]+=`,${numbers[0].join(',')}` : numbers[1]+=`${numbers[0].join(',')}`
+                numbers[0] = [];
+            } else {
+                numbers[1].length ? numbers[1]+=`,${number}` : numbers[1]+=`${number}` 
+            }
         return numbers
-    },[[],""])    
+    },[[],''])[1]
 }
 
 function doRange(arr){
     return`${arr[0]}-${arr[arr.length-1]}` 
 }
 
-console.log(solution([1,2,3,4,5,6,7,8,9,11,12]))
+console.log(solution([]))
