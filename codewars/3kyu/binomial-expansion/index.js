dbfbf3b1ad61dede242840c41ab725429f6d4f1e
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-09-15 02:06:49 -0300

    Thoughts about the problem

--- codewars/3kyu/binomial-expansion/index.js
+++ codewars/3kyu/binomial-expansion/index.js
@@ -1,2 +1,22 @@
 export function expand(expr) {
 }
+
+/* 
+Analyzing the problem:
+- input is always an expression like (ax+b)^n
+- x is any single character variable, 
+- a and b are integers and n is a natural number 
+
+-  If a = 1, no coefficient will be placed in front of the variable. 
+-  If a = -1, a "-" will be placed in front of the variable.
+
+- Expanded form should be returned as a string in the form ax^b+cx^d+ex^f.
+- a, c, and e are the coefficients of the term and x is the variable in the original expression.
+- b, d, and f, are the powers that x is being raised to in each term and are in decreasing order.
+
+- If the coefficient of a term is zero, the term should not be included. 
+- If the coefficient of a term is one, the coefficient should not be included. 
+- If the coefficient of a term is -1, only the "-" should be included. 
+- If the power of the term is 0, only the coefficient should be included. 
+- If the power of the term is 1, the caret and power should be excluded. "(x-1)^1" toBe "x-1"
+*/
