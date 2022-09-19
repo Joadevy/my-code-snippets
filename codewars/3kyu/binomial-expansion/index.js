c8b1263867435edbe50f79e6130b45f4301f852b
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-09-19 02:07:45 -0300

    All terms obtained, working with the final union

--- codewars/3kyu/binomial-expansion/index.js
+++ codewars/3kyu/binomial-expansion/index.js
@@ -1,34 +1,48 @@
 export function expand(expr) {
   let pot = expr.match(/\^([\d]*)$/)[1]; // [1] because the capture group for only the numbers after ^
-  console.log(pot);
   if (pot === 0) return "1";
+
   let coefficient = expr.match(/-?\d*[a-z]/i)[0]; // Matches the char variable with - or not.
   console.log(coefficient);
-  let operationTerm = expr.match(/[\+|\-]([\d]*)/i); // [1] because of the capture group
+  let operationTerm = expr.match(/[a-z]([\+|\-](\d+))/i); // [1] because of the capture group
   operationTerm[0].startsWith("-")
     ? (operationTerm = parseInt(`-${operationTerm[1]}`))
     : (operationTerm = parseInt(operationTerm[1]));
-  console.log(operationTerm);
 
   let expansion = "";
   for (let i = pot; i >= 0; i--) {
-    let coef = getCoefficient(coefficient, i);
-    coef.startsWith("-")
-      ? (expansion += coef)
-      : coef.length > 0 && expansion.length > 0
-      ? (expansion += `+${coef}`)
-      : (expansion += coef);
+    let first = getFirstTerm(pot, i);
+    let second = getSecondTerm(coefficient, i);
+    let third = getThirdTerm(operationTerm, i);
+    // console.log("first es", first);
+    // console.log("second es", second);
+    // console.log("third es", third);
+    // console.log("number es:", getNumber(second));
+    getTerm(first, getNumber(second), third, second);
+    // console.log("producto es: ", second.match(/-?d*/) * first * third);
+    second.startsWith("-")
+      ? (expansion += second)
+      : second.length > 0 && expansion.length > 0
+      ? (expansion += `+${second}`)
+      : (expansion += second);
   }
-  console.log(expansion);
+  return expansion;
 }
 
-function getCoefficient(term, pot) {
+function getFirstTerm(pow, actual) {
+  const dividend = factorial(pow);
+  const divisor = factorial(actual) * factorial(pow - actual);
+  return dividend / divisor;
+}
+
+function getSecondTerm(term, pot) {
   if (pot === 1) return term;
   else if (pot === -1) return `-${term}`;
   else if (pot === 0) return ""; // If coefficient^0 only need the numberTerm.
   const number = term.match(/-?\d*/)[0];
   // Checking to pow the number before the coefficient.
   if (number) {
+    if (number === "-") return `${term}^${pot}`;
     term = term.match(/[a-z]/i);
     return `${Math.pow(parseInt(number), parseInt(pot))}${term}^${pot}`;
   }
@@ -36,15 +50,40 @@ function getCoefficient(term, pot) {
   return `${term}^${pot}`;
 }
 
-function factorial(number) {
-  factorial = 1;
-  for (let i = number; i > 0; i--) {
-    factorial *= i;
-  }
-  return factorial;
+function getThirdTerm(operationTerm, pow) {
+  return Math.pow(parseInt(operationTerm), parseInt(pow));
 }
 
-console.log(expand("(-2X-5)^3"));
+function getNumber(exp) {
+  const aux = exp.match(/-?\d*/)[0];
+  return aux === "" ? 1 : aux === "-" ? -1 : aux;
+}
+
+function getTerm(first, second, third, exp) {
+  let aux = exp.match(/[a-z]\^\d+/i);
+  if (aux !== null) {
+    // console.log("aux es:", aux[0]);
+    console.log(`${first * second * third}${aux}`);
+    return;
+  }
+  aux = exp.match(/[a-z]/i);
+  if (aux !== null) {
+    console.log(`${first * second * third}${aux}`);
+    return;
+  }
+  console.log(`${first * second * third}`);
+}
+
+function factorial(number) {
+  let fact = 1;
+  for (let i = number; i > 0; i--) {
+    fact *= i;
+  }
+  return fact;
+}
+
+console.log(expand("(-2y-1)^4"));
+
 /* 
 Analyzing the problem:
 - input is always an expression like (ax+b)^n
