4ae45ca138ee92ff5d2fbb56bdcc5a6240887031
jjoadevy <jjoaquinarlettaz@gmail.com>
2022-10-11 18:38:22 -0300

    Working w the LIFT!

--- codewars/3kyu/the-lift/index.js
+++ codewars/3kyu/the-lift/index.js
@@ -1,14 +1,36 @@
-// Need a queue for tracking the people waiting for the lift
-
 export const theLift = function (queues, capacity) {
-  const liftQueue = [];
-  for (let floor of queues) {
-    if (floor.length > 0) {
-      enterLift(floor, liftQueue, capacity);
-    }
-  }
+  const historyFloors = [];
+  let liftQueue = [];
+  let actualFloor = 0;
+  let godown = false;
 
-  return liftQueue;
+  // If the flat queue of the building if empty => there's no people waiting for the lift.
+  while (
+    (queues.flat().length || liftQueue.length) &&
+    actualFloor < queues.length &&
+    actualFloor >= 0
+  ) {
+    if (queues[actualFloor].length > 0) {
+      console.log(queues[actualFloor]);
+      enterLift(queues[actualFloor], liftQueue, capacity);
+      historyFloors.push(actualFloor);
+    }
+    if (liftQueue.length && liftQueue.includes(actualFloor)) {
+      liftQueue = liftQueue.filter((person) => person !== actualFloor);
+      historyFloors.push(actualFloor);
+    }
+
+    if (
+      actualFloor >= maxFloorToGo(queues.flat()) &&
+      actualFloor >= maxFloorToGo(liftQueue.flat())
+    )
+      godown = true;
+    // else if (actualFloor <= minFloorToGo(queues.flat())) godown = false;
+    godown ? actualFloor-- : actualFloor++;
+  }
+  historyFloors.push(0);
+  historyFloors.unshift(0);
+  return historyFloors;
 };
 
 function enterLift(peopleInFloor, liftQueue, capacity) {
@@ -17,14 +39,38 @@ function enterLift(peopleInFloor, liftQueue, capacity) {
   }
 }
 
+function maxFloorToGo(queue) {
+  return queue.reduce((max, floor) => {
+    floor > max ? (max = floor) : "";
+    return max;
+  }, 0);
+}
+
+function minFloorToGo(queue) {
+  return queue.reduce((min, floor) => {
+    floor < min ? (min = floor) : "";
+    return min;
+  }, queue.length);
+}
+
+// function goUp(liftQueue, actualfloor, targetFloor, capacity) {
+//   for (let floor = actualfloor; floor <= targetFloor; floor++) {
+//     if (floor.length > 0) {
+//       enterLift(floor, liftQueue, capacity);
+//     }
+//   }
+// }
+
 let queues = [
   [], // G
-  [], // 1
-  [5, 5, 5], // 2
+  [4], // 1
+  [3, 3, 3], // 2
   [], // 3
-  [], // 4
+  [2, 5], // 4
   [], // 5
   [], // 6
 ];
+// console.log(maxFloorToGo(queues.flat()));
+
 console.log(theLift(queues, 5));
 // .toMatchObject([0, 2, 5, 0]);
